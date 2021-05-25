7bb318c31eb87de6b5c549ca045f9032e5437fe4
DDONGNI <de0309@naver.com>
2021-05-25 13:37:32 +0900

    AD 추가, 액션바, 아이콘 수정

--- app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
+++ app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
@@ -2,26 +2,42 @@ package jeongwookdongeun.easyqrcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
+
     private WebView webView; //웹뷰 선언
     private WebSettings webSettings; //웹뷰 세팅
+
+    private AdView adView; //애드뷰 변수 선언
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //웹뷰 시작
         webView = (WebView) findViewById(R.id.qrWebView);
 
-        webView.setWebViewClient(new WebViewClient()); //클릭시 새창 안뜨게
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        }); //클릭시 새창 안뜨게
         webSettings = webView.getSettings(); //세부 세팅 등록
         webSettings.setJavaScriptEnabled(true); //웹페이지 자바스크립트 허용 여부
         webSettings.setSupportMultipleWindows(false); //새창 띄우기 허용 여부
@@ -35,5 +51,39 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
 
         webView.loadUrl("https://nid.naver.com/login/privacyQR");
+
+
+        //재시작 될 때 확인하기 위한 String
+        getIntent().setAction("Already created");
+
+
+        //배너 테스트
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        adView = findViewById(R.id.adView); //배너광고 레이아웃 가져오기
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER); //광고 사이즈는 배너 사이즈로 설정
+        adView.setAdUnitId("\n" + "ca-app-pub-3940256099942544/6300978111");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String action = getIntent().getAction();
+
+        if (action == null || !action.equals("Already created")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else
+            getIntent().setAction(null);
+    }
+
 }
