5fc4e697e7d51ad1108deee3184461a69926cd98
hamletshu <ahngo13@naver.com>
2021-05-25 23:39:34 +0900

    완성

--- app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
+++ app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
@@ -2,8 +2,15 @@ package jeongwookdongeun.easyqrcheckin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,8 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView; //웹뷰 선언
     private WebSettings webSettings; //웹뷰 세팅
+    private CookieManager cookieManager;
 
     private AdView adView; //애드뷰 변수 선언
+    private String cookie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +43,78 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
+//                Uri requested = Uri.parse(url);
+
+                //네트워크 url일 경우
+                if( URLUtil.isNetworkUrl(url) ) {
+                    if("https://m.naver.com/".equals(url)){
+                        //쿠키가 없으면 로그인 페이지
+                        if("".equals(cookie) || cookie == null){
+                            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+                                cookieManager.setAcceptThirdPartyCookies(webView, true);
+                            }
+                            webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+                            //쿠키가 있으면 자동로그인
+                        }else{
+                            webView.loadUrl("https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                try{
+                    startActivity(intent);
+                }catch (ActivityNotFoundException e){
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW);
+                    webIntent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.nhn.android.search"));
+                    if(webIntent.resolveActivity(getPackageManager()) != null){
+                        startActivity(webIntent);
+                    }
+                }
+
                 return true;
             }
-        }); //클릭시 새창 안뜨게
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                    CookieSyncManager.getInstance().sync();
+                }else{
+                    CookieManager.getInstance().flush();
+                }
+            }
+        });
         webSettings = webView.getSettings(); //세부 세팅 등록
         webSettings.setJavaScriptEnabled(true); //웹페이지 자바스크립트 허용 여부
-        webSettings.setSupportMultipleWindows(false); //새창 띄우기 허용 여부
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        webSettings.setSupportMultipleWindows(true); //새창 띄우기 허용 여부
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
         webSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
         webSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
         webSettings.setSupportZoom(false); // 화면 줌 허용 여부
         webSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
         webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+//        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         webSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
 
-        webView.loadUrl("https://nid.naver.com/login/privacyQR");
+        webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR");
 
+        if(cookieManager == null){
+            cookieManager = CookieManager.getInstance();
+            webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+        }
+
+        setCookieAllow(cookieManager, webView);
 
         //재시작 될 때 확인하기 위한 String
         getIntent().setAction("Already created");
 
 
-        //배너 테스트
+        //배너
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -69,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         adView.loadAd(adRequest);
         AdView adView = new AdView(this);
         adView.setAdSize(AdSize.BANNER); //광고 사이즈는 배너 사이즈로 설정
-        adView.setAdUnitId("\n" + "ca-app-pub-3940256099942544/6300978111");
+        adView.setAdUnitId("ca-app-pub-6854372688341522/4174317953");
     }
 
     @Override
@@ -84,6 +141,32 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else
             getIntent().setAction(null);
+    }
+
+    private void setCookieAllow(CookieManager cookieManager, WebView webView) {
+
+        try {
+
+            cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+
+            cookie = cookieManager.getCookie("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR");
+            Log.e("cookie:::", cookie);
+            Log.e("hasCookie:::", cookieManager.hasCookies() + "");
+
+            if("".equals(cookie) || cookie == null){
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+                    cookieManager.setAcceptThirdPartyCookies(webView, true);
+                }
+                webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+            }else{
+                webView.loadUrl("https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
