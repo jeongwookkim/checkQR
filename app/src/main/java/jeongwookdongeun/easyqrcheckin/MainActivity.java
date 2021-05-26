28ac5206cba45ce39a294ab247ef7720ca507d59
DDONGNI <de0309@naver.com>
2021-05-26 16:59:23 +0900

    JAVA 코드 정리

--- app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
+++ app/src/main/java/jeongwookdongeun/easyqrcheckin/MainActivity.java
@@ -24,11 +24,12 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView; //웹뷰 선언
-    private WebSettings webSettings; //웹뷰 세팅
-    private CookieManager cookieManager;
+    private WebView webView;
+    private WebSettings webSettings;
 
-    private AdView adView; //애드뷰 변수 선언
+    private AdView adView;
+
+    private CookieManager cookieManager;
     private String cookie;
 
     @Override
@@ -36,58 +37,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //웹뷰 시작
+        /* 웹뷰 */
         webView = (WebView) findViewById(R.id.qrWebView);
 
-        webView.setWebViewClient(new WebViewClient(){
+        webView.setWebViewClient(new WebViewClient() {
+
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                Uri requested = Uri.parse(url);
-
-                //네트워크 url일 경우
-                if( URLUtil.isNetworkUrl(url) ) {
-                    if("https://m.naver.com/".equals(url)){
-                        //쿠키가 없으면 로그인 페이지
-                        if("".equals(cookie) || cookie == null){
-                            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                                webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-                                cookieManager.setAcceptThirdPartyCookies(webView, true);
-                            }
-                            webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-                            //쿠키가 있으면 자동로그인
-                        }else{
-                            webView.loadUrl("https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-                        }
-                        return true;
-                    }
-                    return false;
-                }
-
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                try{
-                    startActivity(intent);
-                }catch (ActivityNotFoundException e){
-                    Intent webIntent = new Intent(Intent.ACTION_VIEW);
-                    webIntent.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.nhn.android.search"));
-                    if(webIntent.resolveActivity(getPackageManager()) != null){
-                        startActivity(webIntent);
-                    }
-                }
-
-                return true;
+                //네트워크 url일 경우 -> 네이버 홈 으로 갈 경우 return true, 아니면 return false
+                return URLUtil.isNetworkUrl(url) ? ("https://m.naver.com/".equals(url) ? true : false) : true;
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
 
-                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                     CookieSyncManager.getInstance().sync();
-                }else{
+                } else {
                     CookieManager.getInstance().flush();
                 }
             }
+
         });
         webSettings = webView.getSettings(); //세부 세팅 등록
         webSettings.setJavaScriptEnabled(true); //웹페이지 자바스크립트 허용 여부
@@ -98,23 +69,37 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setSupportZoom(false); // 화면 줌 허용 여부
         webSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
         webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
-//        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         webSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
 
-        webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR");
+        webView.loadUrl("https://nid.naver.com/login/privacyQR");
 
-        if(cookieManager == null){
+
+        /* 쿠키 */
+        try {
             cookieManager = CookieManager.getInstance();
-            webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+            cookieManager.setAcceptCookie(true);
+
+            cookie = cookieManager.getCookie("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR");
+
+            //쿠키가 없을 경우
+            if("".equals(cookie) || cookie == null) {
+                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+                    cookieManager.setAcceptThirdPartyCookies(webView, true);
+                }
+            }
+            webView.loadUrl("https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        setCookieAllow(cookieManager, webView);
 
-        //재시작 될 때 확인하기 위한 String
+        /* 재실행 될 때 확인하기 위한 String 저장 */
         getIntent().setAction("Already created");
 
 
-        //배너
+        /* 배너 */
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -134,39 +119,13 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         String action = getIntent().getAction();
-
+        //이미 실행된 적이 있다면 재로드
         if (action == null || !action.equals("Already created")) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
         } else
             getIntent().setAction(null);
-    }
-
-    private void setCookieAllow(CookieManager cookieManager, WebView webView) {
-
-        try {
-
-            cookieManager = CookieManager.getInstance();
-            cookieManager.setAcceptCookie(true);
-
-            cookie = cookieManager.getCookie("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR");
-            Log.e("cookie:::", cookie);
-            Log.e("hasCookie:::", cookieManager.hasCookies() + "");
-
-            if("".equals(cookie) || cookie == null){
-                if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-                    cookieManager.setAcceptThirdPartyCookies(webView, true);
-                }
-                webView.loadUrl("https://nid.naver.com/nidlogin.login?svctype=262144&url=https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-            }else{
-                webView.loadUrl("https://nid.naver.com/login/privacyQR"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 
 }
